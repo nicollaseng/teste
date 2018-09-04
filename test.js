0343ead267669de3ac8115d48064a111ee2b597b
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-09-03 23:42:10 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+function SpaceShip(crew, maxSpeed, shipLenght, cargo, passengers){
+    this.crew = crew
+    this.maxSpeed = maxSpeed
+    this.shipLenght = shipLenght
+    this.cargo = cargo
+    this.passengers = passengers
+}
+
+const MileniumFalcon = new SpaceShip(4, 1050, 34, 100, 6)
+console.log(MileniumFalcon)
